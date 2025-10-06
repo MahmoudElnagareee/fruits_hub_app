f3832a7fd0bc3e24195a9d59df84d6e682db42e2
Mahmoud Elnagar <your-email@example.com>
2025-10-06 20:26:42 +0300

    page view item part 2

--- lib/features/on_bording/presentation/views/widgets/page_view_item.dart
+++ lib/features/on_bording/presentation/views/widgets/page_view_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class PageViewItem extends StatelessWidget {
   const PageViewItem({
@@ -16,6 +17,25 @@ class PageViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        SizedBox(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height * 0.4,
+          child: Stack(
+            children: [
+              Positioned.fill(child: SvgPicture.asset(backgroundImage,fit: BoxFit.fill,)),
+              Positioned(bottom: 0, left: 0, right: 0, child: SvgPicture.asset(image,)),
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Text('تخط',),
+              ),
+            ],
+          ),
+        ),
+        title,
+        Text(subtitle),
+      ],
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 // ignore: depend_on_referenced_packages
 import 'package:device_preview/device_preview.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruits_hub_app/core/helper_function/on_generate_routes.dart';
 import 'package:fruits_hub_app/features/splash/presentation/views/splash_view.dart';
+import 'package:fruits_hub_app/generated/l10n.dart';
 
 void main() {
   runApp(
@@ -17,6 +19,14 @@ class FruitHub extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: [
+                S.delegate,
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: S.delegate.supportedLocales,
+            locale: const Locale('ar'),
       debugShowCheckedModeBanner: false,
       onGenerateRoute: onGenerateRoute,
       initialRoute: SplashView.routeName,
